540249b07f45fef6f9ddb21eafc846836eb5b261
Alfredo <alfredofontinele08@gmail.com>
2022-10-08 20:34:35 -0300

    feat/listTech empty

--- src/components/Main/ListTech/index.jsx
+++ src/components/Main/ListTech/index.jsx
@@ -4,7 +4,7 @@ import * as S from './style'
 export const ListTech = ({ techs, setStatusModalUpdate, setCardCurrent }) => {
     return (
         <S.MainList>
-            {
+            {(techs.length) ? (
                 techs.map(({ id, title, status }) => (
                     (id, title, status) && (
                         <CardTech 
@@ -18,7 +18,12 @@ export const ListTech = ({ techs, setStatusModalUpdate, setCardCurrent }) => {
                         />
                     )
                 ))
-            }        
+            ) : (
+                <>
+                    <h3>Nenhuma Tecnologia foi Cadastrada ainda.</h3>
+                    <p>Quando criar suas tecnologias você pode clicar nos cards para removê-las ou atualizar seu status</p>
+                </>
+            )}        
         </S.MainList>
     )
 }
