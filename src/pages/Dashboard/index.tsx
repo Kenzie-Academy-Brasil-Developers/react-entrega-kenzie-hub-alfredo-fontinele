889fc30e6e38948eb033beaa1f8648829c1bb771
Alfredo <alfredofontinele08@gmail.com>
2022-10-17 09:05:23 -0300

    fix/navigate

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,21 @@
-import { useContext } from "react"
+import { useEffect } from "react"
 import { Container } from "../../components/Container/style"
 import { Header } from "../../components/Header"
 import { Line } from "../../components/LineDivision/style"
 import { Main } from "../../components/Main"
 import { User } from "../../components/User"
+import { useValidation } from './../../context/validation';
 
 export const Dashboard = () => {
+    const { navigate } = useValidation()
+
+    useEffect(() => {
+        const token = localStorage.getItem("@hub:token")
+        if (!token) {
+            navigate("/")
+        }
+    }, [])
+    
     return (
         <>
             <Container>
